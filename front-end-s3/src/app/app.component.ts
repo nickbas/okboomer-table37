24e599e97f8e2599545dfd6dc3b9dd0a4d4e9934
MikeChuck_ <mcjoy465@gmail.com>
2019-12-02 17:14:51 -0800

    Removed router

--- front-end-s3/src/app/app.component.ts
+++ front-end-s3/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import geoData from '../../../data/DC_Buildings_Footprint_4326_test.js';
+import geoData from '../../../../data/DC_Buildings_Footprint_4326_test.js';
 
 let mapboxToken = 'pk.eyJ1IjoidXJiYW5pbnN0aXR1dGUyMDE5IiwiYSI6ImNrM293b2VzaTI1c3IzbmtvZ2thcGE5ZjQifQ.srvBDFJzO8pNOz62SNz7wA'
 
@@ -16,6 +16,8 @@ let apiUrl = 'https://3rz79gbmdl.execute-api.us-east-2.amazonaws.com/dev/geodata
 })
 export class AppComponent {
   title = 'hackathon2019';
+	selectedBuilding: any = {}
+	showBuildingInfo: boolean = false
 	constructor(private http: HttpClient) {}
 
 	ngOnInit() {
@@ -48,17 +50,28 @@ export class AppComponent {
 	  });
 
 		map.on('click', 'polygons', function (e) {
-			console.log("e:", e.features)
-			let center = [e.lngLat.lng, e.lngLat.lat]
-			console.log("center:", center)
 			map.flyTo({center: [e.lngLat.lng, e.lngLat.lat]});
+
+
+			this.selectedBuilding = e.features[0].properties
+			this.showBuildingInfo = true
+
+			console.log("selectedBuilding:", this.selectedBuilding)
+		});
+
+		map.on('mouseenter', 'polygons', function () {
+		map.getCanvas().style.cursor = 'pointer';
+		});
+
+		// Change it back to a pointer when it leaves.
+		map.on('mouseleave', 'polygons', function () {
+		map.getCanvas().style.cursor = '';
 		});
 
 		this.getData()
 	}
 
 	getData(): void {
-		console.log("runnign ")
 		this.http.post(apiUrl, {}).subscribe((response) => {
 			console.log("response:", response)
 		});
